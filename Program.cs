1ffe1ac23a048bfaf60578225019e4485b397fdd
Dirk Dobson <dirkvancouver@live.com>
2019-01-29 17:10:13 -0700

    readonly

--- Program.cs
+++ Program.cs
@@ -1,32 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Fields
 {
-    public class Customer
-    {
-        public int Id;
-        public string Name;
-        public List<Order> Orders;
-
-        public Customer(int id)
-        {
-            this.Id = id;
-        }
-        public Customer(int id, string name)
-            : this(id)
-        {
-            this.Name = name;
-        }
-    }
-    public class Order
-    {
-
-    }
+  
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            customer.Promote();
+
+            System.Console.WriteLine(customer.Orders.Count);
+
         }
     }
 }
